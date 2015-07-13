cd4bf615f7430afa92349da564f200b87679de30
robclu <robclu1818@gmail.com>
2015-07-14 00:35:58 +0200

    Finished (untested) weight update kernel

--- src/layer/types/softmax_kernels_gpu.cuh
+++ src/layer/types/softmax_kernels_gpu.cuh
@@ -24,14 +24,44 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+/*
+ * ==========================================================================================================
+ * Function         : updateWeights 
+ * 
+ * Description      : Updates the weights for a layer using the GPU. The update rule (amount to modify the
+ *                    weight by) is first determined, and is then subtracted from the current value of the
+ *                    weight.
+ *                    
+ * Inputs           : curr_wba      : A pointer to the current wba tensor 
+ *                  : ces           : Current Error Start, which is the endex of the first error element in
+ *                  :               : the curr_wba array
+ *                  : num_errors    : The dimensionality of the erros (how many errors there are, which is the
+ *                  :               : number of elements after ces till the last error element.
+ *                  : prev_wba      : The previous wba tensor
+ *                  : pas           : Previous Activation Start, which has the same meaning as ces but points
+ *                  :               : to where the activations start
+ *                  : num_acts      : The number of elements past pas that are activation elements
+ *                  : prev_wba_delta: An array of the weight update values from the previous iteration
+ *                  : learn_rate    : The learning rate to use for the update
+ *                  : momentum      : The amount of momentum to use for the update
+ * ==========================================================================================================
+ */
 template <typename dType>
-void updateWeights( dType* prev_acts , size_t N         ,
-                    dType* curr_errs , size_t M         ,
-                    dType  learn_rate, dType momentum   ) { 
+void updateWeights( dType* curr_wba       , size_t ces       , size_t num_errs,
+                    dType* prev_wba       , size_t pas       , size_t num_acts,
+                    dType* prev_wba_delta , dType learn_rate , dType momentum ) {
     
+    // Get thread index to use as weight index 
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     
-    // Update weights 
+    // Determine the weight update (might need atomics)
+    dType prev_weight_update = prev_wba_delta[ idx ];
+    prev_weight_delta[ idx ] = momentum * prev_weight_update -              // Momentum contribution
+                                learn_rate *                                // Gradient descent
+                                curr_wba[ ces + ( idx % num_errs ) ] *      // This layers error      
+                                prev_wba[ pas + ( idx / num_acts ) ];       // Prev layer activation
+    // Update the weight with the weight update
+    curr_wba[ idx ] += prev_weight_delts[ idx ];
 }
 
 #endif
